eb70aa70017111257d8dbb77522deefc40dc5477
Jonathan <jonnygim.dev@gmail.com>
2022-06-14 01:22:28 +0900

    fixed untracked files

--- src/main/java/com/plus1250/jobaTrend/JobaTrendApplication.java
+++ src/main/java/com/plus1250/jobaTrend/JobaTrendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobaTrendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JobaTrendApplication.class, args);
 	}
-
 }
