4730bb6cd13204abe0330db8483562ff0ea6c215
Jonathan <jonnygim.dev@gmail.com>
2022-06-14 01:14:35 +0900

    [Create] project

---
+++ src/main/java/com/plus1250/jobaTrend/JobaTrendApplication.java
@@ -0,0 +1,13 @@
+package com.plus1250.jobaTrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobaTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobaTrendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plus1250/jobaTrend/JobaTrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plus1250.jobaTrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobaTrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
