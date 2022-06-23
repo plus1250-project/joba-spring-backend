e111d3bcdd8a7a61e11eb2cd3b4647c96fc59792
Jonathan <jonnygim.dev@gmail.com>
2022-06-23 15:53:39 +0900

    [feature] articleList

--- src/main/java/com/plus1250/jobaTrend/model/entity/ArticleList.java
+++ src/main/java/com/plus1250/jobaTrend/model/entity/ArticleList.java
@@ -1,4 +1,43 @@
 package com.plus1250.jobaTrend.model.entity;
 
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name="Article_list")
 public class ArticleList {
+
+    @Id
+    @Column(name="url")
+    private String articleUrl;
+
+    @Column(name="title")
+    private String articleTitle;
+
+    @Column(name="press_company")
+    private String pubCompany;
+
+    @Column(name="date")
+    private String pubDate;
+
+    @Column(name="industry_name")
+    private String industryName;
+
+    public ArticleList() {}
+
+    public ArticleList(String industryName) {
+        this.industryName = industryName;
+    }
+
+    @Builder
+    public ArticleList(String industryName, String title) {
+        this.industryName = industryName;
+        this.articleTitle = title;
+    }
+
+
 }
--- src/main/java/com/plus1250/jobaTrend/service/ArticleListService.java
+++ src/main/java/com/plus1250/jobaTrend/service/ArticleListService.java
@@ -1,4 +1,9 @@
 package com.plus1250.jobaTrend.service;
 
+import com.plus1250.jobaTrend.model.dto.ArticleListDTO;
+
+import java.util.List;
+
 public interface ArticleListService {
+    List<ArticleListDTO> selectArticleInfo(String industryName);
 }
